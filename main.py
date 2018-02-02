f67e9975eb0f3a844346c2bebea79a0a5e4ac8df
lager6 <lager6@gmail.de>
2018-02-02 14:00:50 +0100

    insert data to db

--- main.py
+++ main.py
@@ -11,11 +11,27 @@ class Example(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     data = db.Column('data', db.Unicode)
 
+    def __init__(self, id, data):
+        self.id = id
+        self.data = data
 
-def main():
+
+def get_all_examples():
     examples = Example.query.all()
     for ex in examples:
         print(ex.id, ex.data)
+    return examples
+
+
+def main():
+    examples = get_all_examples()
+
+    new_id = max([ex.id for ex in examples]) + 1
+    new_example = Example(new_id, "from python")
+    db.session.add(new_example)
+    db.session.commit()
+    examples = get_all_examples()
+
 
 
 if __name__ == '__main__':
