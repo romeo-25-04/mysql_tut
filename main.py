59ae3936fb12c6113c79140136acbd5977c68519
lager6 <lager6@gmail.de>
2018-02-02 13:36:39 +0100

    connect to mysqldb

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:tiger@localhost/mydatabase'
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
 
 class Example(db.Model):
-    __tablename__ = 'example'
+    __tablename__ = 'examples'
     id = db.Column('id', db.Integer, primary_key=True)
     data = db.Column('data', db.Unicode)
 
+
+def main():
+    examples = Example.query.all()
+    for ex in examples:
+        print(ex.id, ex.data)
+
+
+if __name__ == '__main__':
+    main()
